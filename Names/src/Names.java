d594f47226a7e5aea33f06cf5a978c2b74e09569
User <User@DESKTOP-PROO3CA>
2019-04-03 08:52:39 +0500

    Git Names

---
+++ Names/src/Names.java
@@ -0,0 +1,12 @@
+public class Names {
+
+	public static void main(String[] args) {
+		System.out.println("Khabibullina");
+		System.out.println("Lobanova");
+		System.out.println("Safetdinova");
+		System.out.println("Khalisova");
+		System.out.println("Bagautdinova");
+		System.out.println("Fattahova");
+		
+	}
+}
